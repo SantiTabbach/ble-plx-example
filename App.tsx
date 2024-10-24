b40c7cd86eb0f8b5b43eb062656439fc31a47606
Santi Tabbach <tabbach97@gmail.com>
2024-10-24 17:07:28 -0300

    Check permissions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { BluetoothConnectionProvider } from './src/contexts/bluetooth-manager.context';
+import { useEffect } from 'react';
+import { useRequestBlePermissions } from './src/hooks/bluetooth/useRequestBlePermissions';
+import { useBle } from './src/data/ble.store';
+import Home from './src/pages/Home';
 
 export default function App() {
+  const setBlePermissionsState = useBle(
+    (state) => state.setBlePermissionsState
+  );
+  const requestPermission = useRequestBlePermissions();
+
+  useEffect(() => {
+    const checkPermissions = async () => {
+      const grantedPermissions = await requestPermission();
+
+      if (!grantedPermissions) {
+        console.log('Bt permission not granted'); // Show a message or something to the user
+      } else {
+        setBlePermissionsState('granted');
+      }
+
+      return grantedPermissions;
+    };
+
+    checkPermissions();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BluetoothConnectionProvider>
+      <View style={styles.container}>
+        <Home />
+        <StatusBar style="auto" />
+      </View>
+    </BluetoothConnectionProvider>
   );
 }
 
