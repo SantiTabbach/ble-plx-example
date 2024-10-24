d4492c1a6adaa00c9b43341c4ee38a69e22f1bd2
Santi Tabbach <tabbach97@gmail.com>
2024-10-24 17:09:24 -0300

    Add connected device hook

---
+++ src/hooks/bluetooth/useConnectedDevice.ts
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+import type { Device } from 'react-native-ble-plx';
+import { GLUCOSE_SERVICES } from '@/src/constants/bluetooth';
+import { useGlucometerDevice } from '@/src/data/glucometer-device.store';
+import { connectToDeviceLogger } from '@/src/utils/logs';
+
+const useConnectedDevice = () => {
+  const setGlucometerDevice = useGlucometerDevice((state) => state.setDevice);
+
+  const workWithConnectedDevice = useCallback(
+    async (connectedDevice: Device) => {
+      await connectedDevice.discoverAllServicesAndCharacteristics();
+      const services = await connectedDevice.services();
+
+      services.forEach((service) => {
+        if (service.uuid === GLUCOSE_SERVICES.glucose) {
+          setGlucometerDevice(connectedDevice);
+          connectToDeviceLogger('Glucometer device connected successfully');
+        }
+        // else if (service.uuid === FOO_SERVICES.foo){
+        //   setGlucometerDevice(connectedDevice);
+        //   connectToDeviceLogger('Glucometer device connected successfully');
+        // }
+      });
+    },
+    [setGlucometerDevice]
+  );
+
+  return { workWithConnectedDevice };
+};
+
+export default useConnectedDevice;
