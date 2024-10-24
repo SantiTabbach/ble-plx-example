65206d0542ab0841d3b3fcb24ddafb88ae8d0531
Santi Tabbach <tabbach97@gmail.com>
2024-10-24 17:06:17 -0300

    Add bt utility

---
+++ src/utils/bluetooth.ts
@@ -0,0 +1,19 @@
+import type { Characteristic, Device, Service } from 'react-native-ble-plx';
+import { EMPTY_STRING } from '@/src/constants/common';
+import { externalDeviceBrand } from '@/src/constants/device';
+
+export const isYuwellDevice = (device: Device): boolean => {
+  const name = (device.name ?? device.localName ?? EMPTY_STRING).toLowerCase();
+  return name.includes(externalDeviceBrand.toLowerCase());
+};
+
+export const findActionById = (
+  payload: Service[] | Characteristic[],
+  id: string
+) => {
+  const result = payload.find((service) => service.uuid === id);
+  if (!result) {
+    throw new Error('NOT FOUND ' + id);
+  }
+  return result;
+};
