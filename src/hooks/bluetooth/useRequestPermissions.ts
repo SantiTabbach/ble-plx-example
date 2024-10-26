6653fa735baf1f34a5999d279b6d13e51aa976af
Santi Tabbach <tabbach97@gmail.com>
2024-10-26 18:12:21 -0300

    Change hook name

---
+++ src/hooks/bluetooth/useRequestPermissions.ts
@@ -0,0 +1,41 @@
+import * as ExpoDevice from 'expo-device';
+import { PermissionsAndroid, Platform } from 'react-native';
+
+export const useRequestPermissions = () => {
+  const requestAndroid31Permissions = async () => {
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    );
+
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN
+    );
+
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+    );
+
+    return (
+      bluetoothScanPermission === PermissionsAndroid.RESULTS.GRANTED &&
+      bluetoothConnectPermission === PermissionsAndroid.RESULTS.GRANTED &&
+      fineLocationPermission === PermissionsAndroid.RESULTS.GRANTED
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === 'android') {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        return await requestAndroid31Permissions();
+      }
+    } else {
+      return true;
+    }
+  };
+
+  return requestPermissions;
+};
