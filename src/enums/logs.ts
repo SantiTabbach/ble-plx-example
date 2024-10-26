a076a34ba6772470d74d2d715b991c3995c42366
Santi Tabbach <tabbach97@gmail.com>
2024-10-26 11:00:37 -0300

    Remove glucose logs

--- src/enums/logs.ts
+++ src/enums/logs.ts
@@ -1,23 +1,19 @@
 export enum LogKeys {
-	// BleLogKeys
-	startDeviceScan = 'StartDeviceScan',
-	stopDeviceScan = 'StopDeviceScan',
-	connectToDevice = 'ConnectToDevice',
-	disconnectFromDevice = 'DisconnectFromDevice',
-	getDeviceServices = 'GetDeviceServices',
+  // BleLogKeys
+  startDeviceScan = 'StartDeviceScan',
+  stopDeviceScan = 'StopDeviceScan',
+  connectToDevice = 'ConnectToDevice',
+  disconnectFromDevice = 'DisconnectFromDevice',
+  getDeviceServices = 'GetDeviceServices',
 
-	// Glucose
-	syncGlucoseRecords = 'SyncGlucoseRecords',
-	realtimeGlucoseMeasurement = 'RealtimeGlucoseMeasurement',
-
-	// Blood pressure
-	realtimePressureMeasurement = 'RealtimePressureMeasurement',
-	processPressureMeasurementLogger = 'ProcessPressureMeasurementLogger',
+  // Blood pressure
+  realtimePressureMeasurement = 'RealtimePressureMeasurement',
+  processPressureMeasurementLogger = 'ProcessPressureMeasurementLogger',
 }
 
 export enum LogLevel {
-	Error = 'error',
-	Log = 'log',
-	Info = 'info',
-	Warn = 'warn',
+  Error = 'error',
+  Log = 'log',
+  Info = 'info',
+  Warn = 'warn',
 }
