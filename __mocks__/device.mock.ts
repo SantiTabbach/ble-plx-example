b30d53765ffe3979704b1b1d3afbb44b13328c40
Santi Tabbach <tabbach97@gmail.com>
2024-11-08 10:08:52 -0300

    Add Device mocks

---
+++ __mocks__/device.mock.ts
@@ -0,0 +1,13 @@
+import { Device } from 'react-native-ble-plx';
+
+export const DeviceMock = {
+  id: '44:28:A3:65:EE:5C',
+  isConnectable: null,
+  manufacturerData: null,
+  mtu: 247,
+  name: 'Yuwell BP-YE630CR',
+  overflowServiceUUIDs: null,
+  serviceData: null,
+  serviceUUIDs: null,
+  solicitedServiceUUIDs: null,
+} as Device;
