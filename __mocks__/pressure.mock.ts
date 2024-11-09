a7244d7617824512804985f03c4bf150641d8691
Santi Tabbach <tabbach97@gmail.com>
2024-11-09 15:24:06 -0300

    Add pressure mock

--- __mocks__/pressure.mock.ts
+++ __mocks__/pressure.mock.ts
@@ -1,1 +1,4 @@
-export const HexMock = 'HosARwBdAOgHCwgJIgpCAAAAAA==';
+export const HexMock = 'B4sARwBdAOgHCwgJIgpCAAAAAA=='; // Mock with all flags
+export const HexMockNoU = 'BosARwBdAOgHCwgJIgpCAAAAAA=='; // Mock without unit flag
+export const HexMockNoT = 'BYsARwBdAOgHCwgJIgpCAAAAAA=='; // Mock without timestamp flag
+export const HexMockNoH = 'A4sARwBdAOgHCwgJIgpCAAAAAA=='; // Mock without heart rate flag
